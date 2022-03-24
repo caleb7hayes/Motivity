ce1216099c1e8f2b17178faf80ed68bcd7e4b20a
JosiahSchwahn <47616104+JosiahSchwahn@users.noreply.github.com>
2022-03-24 11:09:26 -0600

    Add files via upload
    3.24.22 Josiah's Motivity XCode Project

---
+++ Motivity/Motivity/Motivity/MotivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivityApp.swift
+//  Motivity
+//
+//  Created by Josiah Schwahn on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MotivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
