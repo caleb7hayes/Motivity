d6504d84211f31c0e799d7b8caf40f8f7343befb
cjhess19 <cjhess19@gmail.com>
2022-04-26 16:26:03 -0600

    Oush

--- Motivity/MotivityWidget/MotivityWidget.swift
+++ Motivity/MotivityWidget/MotivityWidget.swift
@@ -13,39 +13,39 @@ import Firebase
 
 
 
-struct Provider: IntentTimelineProvider {
+struct Provider: TimelineProvider {
     
-    @AppStorage("events", store: UserDefaults(suiteName: "Team-Motivity.Motivity"))
-    var eventData: Data = Data()
+//    @AppStorage("events", store: UserDefaults(suiteName: "Team-Motivity.Motivity"))
+//    var eventData: Data = Data()
     
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+        SimpleEntry(date: Date())
     }
 
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date())
         completion(entry)
     }
 
-    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+//        print("updating timeline")
         var entries: [SimpleEntry] = []
 
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
+//        let midnight = Calendar.current.startOfDay(for: currentDate)
+//        let nextMidnight = Calendar.current.date(byAdding: .day, value: 1, to: midnight)
+//        for offset in 0 ..< 60 * 24{
+        let entryDate = SimpleEntry(date: currentDate)
+        entries.append(entryDate)
+//        }
+        let refreshDate = Calendar.current.date(byAdding: .minute, value: 2, to: Date())!
+        let timeline = Timeline(entries: entries, policy: .after(refreshDate))
         completion(timeline)
-    }
+        }
 }
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
-    let configuration: ConfigurationIntent
 }
 
 struct MotivityWidgetEntryView : View {
@@ -57,7 +57,7 @@ struct MotivityWidgetEntryView : View {
     var body: some View {
         switch family {
         case .systemSmall:
-            MotivityWidgetSmallView(entry: Provider.Entry.init(date: Date(), configuration: ConfigurationIntent()))
+            MotivityWidgetSmallView(entry: Provider.Entry.init(date: Date()))
         case .systemMedium:
             MotivityWidgetMediumView()
         case .systemLarge:
@@ -70,62 +70,25 @@ struct MotivityWidgetEntryView : View {
     }
 }
 
-func getDates() -> String {
-//    print("IN GET DATES")
-////    let events = UserDefaults.group?.object(forKey: "EventsKey")
-//    var events : [String:[String:Any]] = [:]
-////    var allEvents = String()
-    let router = AuthRouter()
-    let events = router.getEvents()
-
-//    guard let dur = events["School"]["Duration"] as? String else {
-//        return "No Data"
-//    }
-//    for key in events.keys {
-//        allEvents += key
-//    }
-//    for key in events.keys {
-//        allEvents += key
-//
-//    }
-//
-//    return allEvents
-    return events
-}
-
 // struct for functionality of small widget
 struct MotivityWidgetSmallView : View {
-    let events = UserDefaults.group?.object(forKey: "EventsKey")
-    var eventData = getDates()
     var entry: Provider.Entry
     var body: some View {
-        
         ZStack {
             Image("Motivity Login Background").ignoresSafeArea()
-            Text(eventData)
-            Text(entry.date, style: .timer)
-                .multilineTextAlignment(.center)
-            
-//            Text(events)
-        }
-
-        
-//        ZStack{
-//
-//            //background image for all pages throughout Motvitiy
-//
-//            //vertical stack for top text
-//            VStack(alignment: .leading){
-//
-//
-//
-////                    .font(.body)
-////                    .fontWeight(.bold)
-////                    .foregroundColor(Color.white)
-////                    .frame(maxWidth: .infinity, alignment: .leading)
-////                    .padding(.leading,70)
+                .padding(.leading, 20)
+//            if eventData.signedIn{
+//                Text("Signed in")
 //            }
-//        }
+//            else{
+//                Text("Not Signed in \n Click here to sign in!")
+//                    .foregroundColor(Color.white)
+//                    .padding(.leading, 5)
+//            }
+            
+            Text(entry.date, style: .timer).ignoresSafeArea()
+
+        }
     }
 }
 
@@ -185,12 +148,10 @@ struct MotivityWidgetExtraLargeView : View {
 @main
 struct MotivityWidget: Widget {
     
-    
-    
     let kind: String = "MotivityWidget"
 
     var body: some WidgetConfiguration {
-        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
             MotivityWidgetEntryView(entry: entry)
         }
         .configurationDisplayName("Motivity")
@@ -201,7 +162,7 @@ struct MotivityWidget: Widget {
 
 struct MotivityWidget_Previews: PreviewProvider {
     static var previews: some View {
-        MotivityWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+        MotivityWidgetEntryView(entry: SimpleEntry(date: Date()))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
